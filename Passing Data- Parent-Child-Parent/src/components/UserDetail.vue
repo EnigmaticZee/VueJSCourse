7ac247cceeae13b2077f702f5356096aaf4e3a08
EnigmaticZee <zaidshakilz@gmail.com>
2017-10-20 18:31:46 +1100

    Passing Data - PCP
    Parent to Child to Parent
    
    Two ways
    - Using Emit
    - Using Callback

---
+++ Passing Data- Parent-Child-Parent/src/components/UserDetail.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{switchName()}}</p>
+        <button @click="resetName">Reset</button>
+        <button @click="resetFn">Reset</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        //To validate props it must be an object
+        props: {
+            //name: String
+            myName: {
+                type: String
+            },
+            //custom event
+            resetFn: Function
+        },
+        methods: {
+            switchName(){
+                return this.myName;
+            },
+            resetName(){
+                this.myName = 'Zaid';
+                //Emit the change to the parent
+                this.$emit('nameWasReset', this.myName);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
